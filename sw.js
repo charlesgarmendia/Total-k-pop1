8e0e99355094776183addf0246cc57171492a2bf
charlesgarmendia <cash19617309@gmail.com>
2026-01-30 10:15:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Incrementa este número (v1.1, v1.18, etc.) cada vez que hagas un cambio en el HTML/CSS
+// Incrementa este número (v1.1, v1.19, etc.) cada vez que hagas un cambio en el HTML/CSS
 const CACHE_NAME = 'total-kpop-v1.1';
 const assets = [
   '/',
