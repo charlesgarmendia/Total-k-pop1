9e5415dc18487559b191bcb4e03789d59e3ae7c6
charlesgarmendia <cash19617309@gmail.com>
2026-01-30 22:34:32 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,73 @@
-// sw.js MEJORADO
-const CACHE_VERSION = 'kpop-master-v1.1';
-const STATIC_CACHE = `${CACHE_VERSION}-static`;
-const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
-
-const staticAssets = [
-  './',
-  './index.html',
-  './manifest.json',
-  // Agrega aquí otros assets estáticos
+const CACHE_NAME = 'fans-kpop-v1.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icons/icon-72.png',
+  '/icons/icon-96.png',
+  '/icons/icon-128.png',
+  '/icons/icon-144.png',
+  '/icons/icon-152.png',
+  '/icons/icon-192.png',
+  '/icons/icon-384.png',
+  '/icons/icon-512.png'
 ];
 
+// Instalar Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(STATIC_CACHE)
-      .then(cache => cache.addAll(staticAssets))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(cachedResponse => {
-        if (cachedResponse) return cachedResponse;
-        
-        return fetch(event.request).then(fetchResponse => {
-          return caches.open(DYNAMIC_CACHE).then(cache => {
-            cache.put(event.request.url, fetchResponse.clone());
-            return fetchResponse;
-          });
-        });
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
       })
   );
 });
 
+// Activar Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Eliminando cache antiguo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
-
-
-
-
-
-
+// Fetch de recursos
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - devolver respuesta
+        if (response) {
+          return response;
+        }
+        
+        // Clonar la request
+        const fetchRequest = event.request.clone();
+        
+        return fetch(fetchRequest).then(response => {
+          // Verificar si la respuesta es válida
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          // Clonar la respuesta
+          const responseToCache = response.clone();
+          
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+            
+          return response;
+        });
+      })
+  );
+});
